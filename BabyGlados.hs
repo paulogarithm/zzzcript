7110e16b6e240473b714231a9660a34c825ed246
Paul PARISOT <paul.parisot@epitech.eu>
2024-09-02 23:31:05 +0200

    fix: parseSum
    * make it functionnal

--- BabyGlados.hs
+++ BabyGlados.hs
@@ -3,6 +3,8 @@
 import System.Environment
 import System.Exit
 import Data.List
+import Data.Char
+import Text.Read(reads)
 
 data Symbol = While | If | Else |
     LBra | LPar | RBra | RPar |
@@ -46,28 +48,70 @@ getSymStr str = case (find (\(x,_) -> isPrefixOf x str) symbolList) of
     Just (symstr,sym) -> Just (sym, drop (length symstr) str)
     Nothing -> Nothing
 
-loopAction :: String -> [Symbol] -> Maybe [Symbol]
-loopAction [] list = Just list
-loopAction (' ':xs) list = loopAction xs list
-loopAction ('\n':xs) list = loopAction xs list
-loopAction (x:xs) list = case (getSymChar x) of
+getSymUser :: String -> Maybe (Symbol,String)
+getSymUser s = case (reads s :: [(Float, String)]) of
+    [(n,rest)] -> Just (Number n,rest)
+    _ -> case (span isAlphaNum s) of
+        (var, rest)
+            | not (null var) && isAlphaNum (head var) -> Just (Id var,rest)
+            | otherwise -> Nothing
+
+getAllSyms :: String -> [Symbol] -> Maybe [Symbol]
+getAllSyms [] list = Just list
+getAllSyms (' ':xs) list = getAllSyms xs list
+getAllSyms ('\n':xs) list = getAllSyms xs list
+getAllSyms (x:xs) list = case (getSymChar x) of
     Just (Eof) -> Just list
-    Just sym -> loopAction xs (list++[sym])
+    Just sym -> getAllSyms xs (list++[sym])
     Nothing -> case (getSymStr (x:xs)) of
-        Just (ssym,rest) -> loopAction rest (list++[ssym])
-        Nothing -> Nothing
+        Just (sym,rest) -> getAllSyms rest (list++[sym])
+        Nothing -> case (getSymUser (x:xs)) of
+            Just (sym,rest) -> getAllSyms rest (list++[sym])
+            Nothing -> Nothing
 
 -- Parsing
 
-newNode :: Token -> Node
-newNode t = Node (t,(NullNode,NullNode,NullNode))
+type ParsingRes = Maybe (Node,[Symbol])
 
-parseTerm :: [Symbol] -> Maybe (Node,[Symbol])
-parseTerm [] -> Nothing
+newNode :: Token -> Node
+newNode t = Node (t, (NullNode, NullNode, NullNode))
+
+giveResult :: Token -> [Symbol] -> ParsingRes
+giveResult t l = Just (newNode t, l)
+
+parseTerm :: [Symbol] -> ParsingRes
+parseTerm [] = Nothing
 parseTerm (x:xs) = case (x) of
-    Id s -> Just (newNode (Var s),xs)
-    Const c -> Just (newNode (Const c),xs)
+    Id s -> giveResult (TVar s) xs
+    Number c -> giveResult (TConst c) xs
     _ -> Nothing
+
+_parseSumloop :: [Symbol] -> Node -> ParsingRes
+_parseSumloop (Plus:xs) n = case (parseTerm xs) of
+    Just (newn, xxs) -> _parseSumloop xxs (Node (TAdd, (n, newn, NullNode)))
+    Nothing -> Nothing
+_parseSumloop (Minus:xs) n = case (parseTerm xs) of
+    Just (newn, xxs) -> _parseSumloop xxs (Node (TSub, (n, newn, NullNode)))
+    Nothing -> Nothing
+_parseSumloop l n = Just (n, l)
+
+parseSum :: [Symbol] -> ParsingRes
+parseSum [] = Nothing
+parseSum l = case (parseTerm l) of
+    Nothing -> Nothing
+    Just (n,xs) -> case (_parseSumloop xs n) of
+        Nothing -> Nothing
+        x -> x
+
+parsing :: String -> ParsingRes
+parsing s = case (getAllSyms s []) of
+    Just syms -> parseSum syms
+    Nothing -> Nothing
+
+-- parseParen :: [Symbol] -> Maybe (Node, [Symbol])
+-- parseParen [] = Nothing
+-- parseParen (LPar:xs) = 
+-- parseParen _ = Nothing
 
 main :: IO()
 main = putStrLn "hello"
