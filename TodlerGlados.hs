e00d954acf34b0f74a991fd8b090703fdbf43408
Paul PARISOT <paul.parisot@epitech.eu>
2024-09-19 15:02:05 +0200

    feat(todler): finish the tree thingy

--- TodlerGlados.hs
+++ TodlerGlados.hs
@@ -21,6 +21,7 @@ data SymOperator = SoPlus
 data SymTest = SoLT
     | SoGT
     | SoEqual
+    | SoDiff
     deriving(Show)
 
 data Symbol = SymSet
@@ -47,7 +48,8 @@ symOpeList = [("(", SymPar DirOpen), (")", SymPar DirClose),
         ("{", SymBra DirOpen), ("}", SymBra DirClose), ("+", SymOpe SoPlus),
         ("-", SymOpe SoMinus), ("<", SymTest SoLT), (">", SymTest SoGT),
         ("==", SymTest SoEqual), ("=", SymSet), ("/", SymOpe SoDiv),
-        ("*", SymOpe SoMul), (";", SymComma), (",", SymSep)
+        ("*", SymOpe SoMul), (";", SymComma), (",", SymSep),
+        ("!=", SymTest SoDiff)
     ]
 
 symGetValue :: String -> Either String (Symbol, String)
@@ -101,18 +103,18 @@ parseTerm (SymBool b:xs) = Right (emptyNode $ TokBool b, xs)
 parseTerm (SymNum n:xs) = Right (emptyNode $ TokNum n, xs)
 parseTerm sx@(SymDef d:SymPar DirOpen:xs) = parseCall sx
 parseTerm (SymDef d:xs) = Right (emptyNode $ TokDef d, xs)
-parseTerm _ = Left "parsing: nothing for now."
+parseTerm xs = parseParen xs
 
 -- operation ::= <term> | <operation> <opesym> <term>
 parseOperation' :: [Symbol] -> Node -> Either String (Node, [Symbol])
 parseOperation' ((SymOpe x):xs) n = case (parseTerm xs) of
     Right (nn, ys) -> parseOperation' ys (Node (TokOpe x, [n, nn]))
-    Left err -> Left ("parseOperation -> " ++ err)
+    Left err -> Left ("operation -> " ++ err)
 parseOperation' l n = Right (n, l)
 
 parseOperation :: [Symbol] -> Either String (Node, [Symbol])
 parseOperation l = case (parseTerm l) of
-    Left err -> Left ("parseOperation -> " ++ err)
+    Left err -> Left ("operation -> " ++ err)
     Right (n,xs) -> case (parseOperation' xs n) of
         Left err -> Left err
         Right x -> Right x
@@ -121,12 +123,12 @@ parseOperation l = case (parseTerm l) of
 parseTest' :: [Symbol] -> Node -> Either String (Node, [Symbol])
 parseTest' ((SymTest x):xs) n = case (parseOperation xs) of
     Right (nn, ys) -> parseTest' ys (Node (TokTest x, [n, nn]))
-    Left err -> Left ("parseTest -> " ++ err)
+    Left err -> Left ("test -> " ++ err)
 parseTest' l n = Right (n, l)
 
 parseTest :: [Symbol] -> Either String (Node, [Symbol])
 parseTest sx = case (parseOperation sx) of
-    Left err -> Left ("parseTest -> " ++ err)
+    Left err -> Left ("test -> " ++ err)
     Right (n, xs) -> case (parseTest' xs n) of
         Left err -> Left err
         Right x -> Right x
@@ -134,7 +136,7 @@ parseTest sx = case (parseOperation sx) of
 -- call ::= <def> ( <test>, <test>, ... )
 parseCallChildren :: [Symbol] -> Either String ([Node], [Symbol])
 parseCallChildren sx = case (parseTest sx) of
-    Left err -> Left ("parseCall -> " ++ err)
+    Left err -> Left ("call -> " ++ err)
     Right (n, (SymPar DirClose:xs)) -> Right ([n], xs)
     Right (n, (SymSep:xs)) -> case (parseCallChildren xs) of
         Left err -> Left err
@@ -144,21 +146,82 @@ parseCall :: [Symbol] -> Either String (Node, [Symbol])
 parseCall (SymDef f:SymPar DirOpen:SymPar DirClose:xs) =
     Right (emptyNode $ TokCall f, xs)
 parseCall (SymDef f:SymPar DirOpen:xs) = case (parseCallChildren xs) of
-    Left err -> Left ("parseCall -> " ++ err)
+    Left err -> Left err
     Right (nx, xs) -> Right (Node(TokCall f, nx), xs)
-parseCall _ = Left "parseCall: expected 'func(...)'."
+parseCall _ = Left "call: expected 'func(...)'."
 
--- declar ::= <def> = <test>
-parseDeclar :: [Symbol] -> Either String (Node, [Symbol])
-parseDeclar (SymDef d:SymSet:xs) = case (parseTest xs) of
-    Left err -> Left ("parseDeclar -> " ++ err)
-    Right (n, xs) -> Right (Node(TokSet d, [n]), xs)
+-- statement ::= <def> = <test> | <call>
+parseStatement :: [Symbol] -> Either String (Node, [Symbol])
+parseStatement (SymDef d:SymSet:xs) = case (parseTest xs) of
+    Left err -> Left ("statement -> " ++ err)
+    Right (n, (SymComma:xs)) -> Right (Node(TokSet d, [n]), xs)
+    Right _ -> Left $ "declaration of " ++ show d ++ ": expected ';'."
+parseStatement sx@(SymDef d:xs) = case (parseCall sx) of
+    Left err -> Left $ "statement -> " ++ err
+    Right (n, (SymComma:xs)) -> Right (n, xs)
+    Right _ -> Left $ "call of " ++ show d ++ ": expected ';'."
+parseStatement _ = Left "statement: invalid token."
+
+-- paren ::= ( <test> )
+parseParen :: [Symbol] -> Either String (Node, [Symbol])
+parseParen (SymPar DirOpen:xs) = case (parseTest xs) of
+    Left err -> Left $ "parenthesis -> " ++ err
+    Right (n, (SymPar DirClose:xs)) -> Right (n, xs)
+    Right _ -> Left "parenthesis: expected ')'."
+parseParen _  = Left "parenthesis: expected '('."
+
+-- ifstat ::= if <paren> <block> +? else <block>
+parseElse :: [Symbol] -> Either String (Maybe (Node, [Symbol]))
+parseElse (SymKW SkElse:xs) = case (parseBlock xs) of
+    Left err -> Left $ "else -> " ++ err
+    Right (n, xs) -> Right (Just (n, xs))
+parseElse xs = Right Nothing
+
+parseIf :: [Symbol] -> Either String (Node, [Symbol])
+parseIf (SymKW SkIf:xs) = case (parseParen xs) of
+    Right (c, xs) -> case (parseBlock xs) of
+        Right (b, xs) -> case (parseElse xs) of
+            Right Nothing -> Right (Node(TokIf, [c, b]), xs)
+            Right (Just (e, xs)) -> Right (Node(TokIf, [c, b, e]), xs)
+            Left err -> Left err
+        Left err -> Left $ "if body -> " ++ err
+    Left err -> Left $ "if condition -> " ++ err
+
+-- line ::= <ifstat> | <statament>
+parseLine :: [Symbol] -> Either String (Node, [Symbol])
+parseLine sx@(SymKW SkIf:xs) = parseIf sx
+parseLine xs = parseStatement xs
+
+-- block ::= { <line> <line> ... } | <line>
+parseBlock' :: [Symbol] -> Either String ([Node], [Symbol])
+parseBlock' (SymBra DirClose:xs) = Right ([], xs)
+parseBlock' xs = case (parseLine xs) of
+    Right (n, xs) -> case (parseBlock' xs) of
+        Right (content, rest) -> Right (n:content, rest)
+        Left err -> Left err
+    Left err -> Left $ "block -> " ++ err
+
+parseBlock :: [Symbol] -> Either String (Node, [Symbol])
+parseBlock (SymBra DirOpen:xs) = case (parseBlock' xs) of
+    Left err -> Left $ "block -> " ++ err
+    Right (nodes, xs) -> Right (Node(TokBlock, nodes), xs)
+parseBlock xs = parseLine xs
+
+-- main
 
 parseThisPlease :: String -> Either String Node
 parseThisPlease s = case (symGetAll s) of
-    Right sx -> case (parseDeclar sx) of
+    Right sx -> case (parseBlock sx) of
         Right (n, _) -> Right n
         Left err -> Left ("parseThisPlease -> " ++ err)
     Left err -> Left ("parseThisPlease -> " ++ err)
 
+dispTree :: [Node] -> Int -> IO()
+dispTree [] n = return()
+dispTree (Node(t, cx):xs) n = (putStrLn $ (replicate n ' ') ++ (show t))
+    >> dispTree cx (n + 4) >> dispTree xs n
 
+prettyParser :: String -> IO()
+prettyParser s = case (parseThisPlease s) of
+    Left err -> putStrLn $ "[ Exception Caught ] in " ++ err
+    Right tree -> dispTree [tree] 0
